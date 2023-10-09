4fc4d96c007ca555b4f1c950ff2e52b0c6ad92ef
kalinggapadelmuhamad <enginerbros@gmail.com>
2023-10-09 23:56:06 +0700

    Update: add MainScreen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,1 +1,102 @@
+import 'package:flutter/material.dart';
 
+var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset('images/farm-house.jpg'),
+            Container(
+              // color: Colors.amber,
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Staaatliches',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              // color: Colors.blue,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.calendar_today),
+                      const SizedBox(height: 8.0),
+                      Text('Open Everyday', style: informationTextStyle),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.watch_later_outlined),
+                      const SizedBox(height: 8.0),
+                      Text('09:00 - 20:00', style: informationTextStyle)
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.monetization_on),
+                      const SizedBox(height: 8.0),
+                      Text('RP 25.000', style: informationTextStyle)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset('images/farmhouse-lembang.jpg'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset('images/photo3jpg.jpg'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset('images/liburan-di-farmhouse.jpg'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        // ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_bandung/detail_screen.dart';
+import 'package:wisata_bandung/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,106 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Image.asset('images/farm-house.jpg'),
-            Container(
-              // color: Colors.amber,
-              margin: const EdgeInsets.only(top: 16.0),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              // color: Colors.blue,
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.watch_later_outlined),
-                      SizedBox(height: 8.0),
-                      Text('09:00 - 20:00')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8.0),
-                      Text('RP 25.000')
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-            SizedBox(
-              height: 150,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset('images/farmhouse-lembang.jpg'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset('images/photo3jpg.jpg'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset('images/liburan-di-farmhouse.jpg'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-        // ),
-      )),
+      home: const MainScreen(),
     );
   }
 }
