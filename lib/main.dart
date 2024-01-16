129a0d713d6a9ad0264041a070e984b6bf5c7301
kalinggapadelmuhamad <enginerbros@gmail.com>
2024-01-16 16:31:11 +0700

    Update view detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_bandung/detail_screen.dart';
 import 'package:wisata_bandung/main_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
